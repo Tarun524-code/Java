60c29374c2b852af683f6cc3b982b50075a39a1a
Tarun <tarunsriramnambada@gmail.com>
2026-06-24 15:10:17 +0530

    Directed Graph with AdjacenyList

--- Graphs/DGAm.java
+++ Graphs/DGAm.java
@@ -24,6 +24,87 @@ class DGAm {
         }
     }
 
+    public void dfs(int s, boolean visited[]) {
+        visited[s] = true;
+        System.out.print(s + " ");
+
+        for (int i = 0; i < n; i++) {
+            if (m[s][i] == 1 && !visited[i]) {
+                dfs(i, visited);
+            }
+        }
+    }
+
+    public List<Integer> dfsIterative(int s, boolean[] visited) {
+        List<Integer> res = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        stack.push(s);
+
+        while (!stack.isEmpty()) {
+            int u = stack.pop();
+            if (visited[u]) {
+                continue;
+            }
+            visited[u] = true;
+            res.add(u);
+
+            for (int i = 0;i<n;i++) {
+                if (m[u][i] == 1 && !visited[i]) {
+                    stack.push(i);
+                }
+            }
+        }
+        return res;
+    }
+
+    public void search(Scanner sc) {
+        while (true) {
+            System.out.print("Enter starting vertex for DFS (-1 to quit): ");
+            int s = sc.nextInt();
+            if (s == -1) {
+                break;
+            }
+            if (s < 0 || s >= n) {
+                System.out.println("Invalid start vertex");
+                continue;
+            }
+
+            while (true) {
+                System.out.println("1. With Recursion");
+                System.out.println("2. Without Recursion");
+                System.out.println("3. Exit DFS menu");
+                System.out.print("Choose option: ");
+                int ch = sc.nextInt();
+
+                switch (ch) {
+                    case 1: {
+                        boolean[] visited = new boolean[n];
+                        System.out.println("DFS traversal (recursion) starting from vertex " + s + ": ");
+                        dfs(s, visited);
+                        System.out.println();
+                        break;
+                    }
+                    case 2: {
+                        boolean[] visited = new boolean[n];
+                        List<Integer> res = dfsIterative(s, visited);
+                        System.out.println("DFS traversal (iterative) starting from vertex " + s + ": ");
+                        for (int v : res) {
+                            System.out.print(v + " ");
+                        }
+                        System.out.println();
+                        break;
+                    }
+                    case 3:
+                        System.out.println("Exiting DFS menu...");
+                        return;
+                    default:
+                        System.out.println("Invalid choice, try again.");
+                }
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of vertices: ");
@@ -42,6 +123,9 @@ class DGAm {
         }
 
         g.display();
+
+        g.search(sc);
+
         sc.close();
     }
 }
