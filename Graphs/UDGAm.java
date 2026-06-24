17bd7370a8ff571da6286c1b820c4bac02eeb8d7
Tarun <tarunsriramnambada@gmail.com>
2026-06-24 14:48:39 +0530

    Undirected Graph Adjacent Matrix

--- Graphs/UDGAm.java
+++ Graphs/UDGAm.java
@@ -25,6 +25,79 @@ class UDGAm {
         }
     }
 
+    public void dfs(int s, boolean visited[]) {
+        visited[s] = true;
+        System.out.print(s + " ");
+
+        for (int i = 0; i < n; i++) {
+            if (m[s][i] == 1 && !visited[i]) {
+                dfs(i, visited);
+            }
+        }
+    }
+
+    public void dfsIterative(int s) {
+        boolean[] visited = new boolean[n];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(s);
+
+        while (!stack.isEmpty()) {
+            int u = stack.pop();
+            if (visited[u]) {
+                continue;
+            }
+            visited[u] = true;
+            System.out.print(u + " ");
+
+            for (int i = n - 1; i >= 0; i--) {
+                if (m[u][i] == 1 && !visited[i]) {
+                    stack.push(i);
+                }
+            }
+        }
+    }
+
+    public void search(Scanner sc) {
+    while (true) {
+        System.out.print("Enter starting vertex for DFS (-1 to quit): ");
+        int s = sc.nextInt();
+        if (s == -1) {
+            break;
+        }
+        if (s < 0 || s >= n) {
+            System.out.println("Invalid start vertex");
+            continue;
+        }
+
+        boolean[] visited = new boolean[n];
+        System.out.println("DFS traversal starting from vertex " + s + ": ");
+
+        while (true) {
+            System.out.println("1. With Recursion");
+            System.out.println("2. Without Recursion");
+            System.out.println("3. Exit DFS menu");
+            int ch = sc.nextInt();
+
+            switch (ch) {
+                case 1:
+                    dfs(s, visited);
+                    System.out.println();
+                    break;
+                case 2:
+                    dfsIterative(s);
+                    System.out.println();
+                    break;
+                case 3:
+                    System.out.println("Exiting DFS menu...");
+                    return;
+                default:
+                    System.out.println("Invalid choice, try again.");
+            }
+        }
+    }
+}
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of vertices: ");
@@ -43,6 +116,9 @@ class UDGAm {
         }
 
         g.display();
+
+        g.search(sc);
+
         sc.close();
     }
 }
