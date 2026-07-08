2001a61e3df30307b877a8cc0fa45f27e9cdfded
Tarun <tarunsriramnambada@gmail.com>
2026-07-08 22:20:48 +0530

    Pattern Matching Algorithm using BoyerMoore with Good Suffix Heuristic

---
+++ PatternMatching/BoyerMooreGoodSuffix.java
@@ -0,0 +1,64 @@
+package PatternMatching;
+import java.util.*;
+class BoyerMooreGoodSuffix {
+    private static int[] buildGoodSuffixTable(String pattern) {
+        int m = pattern.length();
+        int[] goodSuffix = new int[m];
+        int[] suffix = new int[m];
+        suffix[m - 1] = m;
+        for (int i = m - 2; i >= 0; i--) {
+            int j = i;
+            while (j >= 0 && pattern.charAt(j) == pattern.charAt(m - 1 - (i - j))) {
+                j--;
+            }
+            suffix[i] = i - j;
+        }
+        Arrays.fill(goodSuffix, m);
+        int j = 0;
+        for (int i = m - 1; i >= 0; i--) {
+            if (suffix[i] == i + 1) {
+                for (; j < m - 1 - i; j++) {
+                    if (goodSuffix[j] == m) {
+                        goodSuffix[j] = m - 1 - i;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i <= m - 2; i++) {
+            goodSuffix[m - 1 - suffix[i]] = m - 1 - i;
+        }
+        return goodSuffix;
+    }
+
+    public static void search(String text, String pattern) {
+        int m = pattern.length();
+        int n = text.length();
+        int[] goodSuffix = buildGoodSuffixTable(pattern);
+        int shift = 0;
+        boolean found = false;
+        while (shift <= n - m) {
+            int j = m - 1;
+            while (j >= 0 && pattern.charAt(j) == text.charAt(shift + j)) {
+                j--;
+            }
+            if (j < 0) {
+                System.out.println("Pattern found at index " + shift);
+                found = true;
+                shift += goodSuffix[0];
+            } else {
+                shift += goodSuffix[j];
+            }
+        }
+        if (!found) {
+            System.out.println("Pattern not found in the text.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        String pattern = sc.nextLine();
+        search(text, pattern);
+        sc.close();
+    }
+}
