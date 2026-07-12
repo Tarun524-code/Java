bd630850f6231cb49835875bb0c5bf52be952a84
Tarun <tarunsriramnambada@gmail.com>
2026-07-12 18:41:20 +0530

    .git

--- PatternMatching/BoyerMooreGoodSuffix.java
+++ PatternMatching/BoyerMooreGoodSuffix.java
@@ -56,9 +56,9 @@ class BoyerMooreGoodSuffix {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String t = sc.nextLine();
-        String p = sc.nextLine();
-        search(t, p);
+        String text = sc.nextLine();
+        String pattern = sc.nextLine();
+        search(text, pattern);
         sc.close();
     }
 }
