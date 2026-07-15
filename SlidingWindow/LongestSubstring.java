cddef9857c1fc6edb96b244130e7a6b840163216
Tarun <tarunsriramnambada@gmail.com>
2026-07-15 21:18:31 +0530

    Longest Substring in the given String

---
+++ SlidingWindow/LongestSubstring.java
@@ -0,0 +1,35 @@
+package SlidingWindow;
+import java.util.*;
+
+public class LongestSubstring {
+    public static int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int left = 0, maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+
+            if (map.containsKey(c)) {
+                left = Math.max(left, map.get(c) + 1);
+            }
+
+            map.put(c, right);
+
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        int result = lengthOfLongestSubstring(input);
+        System.out.println("Length of longest substring without repeating characters = " + result);
+
+        sc.close();
+    }
+}
