5fe237ccfd4cb25cc24226a0b9ed3858cea95456
Tarun <tarunsriramnambada@gmail.com>
2026-07-16 21:13:35 +0530

    Longest Common Subsequence Problem using DP approach

---
+++ SlidingWindow/LCS.java
@@ -0,0 +1,34 @@
+package SlidingWindow;
+
+import java.util.Scanner;
+
+public class LCS {
+    public static int lcs(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first string: ");
+        String s1 = sc.nextLine();
+        System.out.print("Enter second string: ");
+        String s2 = sc.nextLine();
+
+        int result = lcs(s1, s2);
+        System.out.println("Length of LCS: " + result);
+        sc.close();
+    }
+}
