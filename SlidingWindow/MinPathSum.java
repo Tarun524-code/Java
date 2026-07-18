80b43f80073ccba23719aea0e199d6c42b0c3b01
Tarun <tarunsriramnambada@gmail.com>
2026-07-18 19:53:25 +0530

    Minimum Path Sum using Dynamic Programming

---
+++ SlidingWindow/MinPathSum.java
@@ -0,0 +1,35 @@
+package SlidingWindow;
+import java.util.Scanner;
+
+public class MinPathSum {
+    public static int minPathSum(int[][] grid, int n, int m) {
+        int[][] dp = new int[n][m];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < n; i++) dp[i][0] = dp[i - 1][0] + grid[i][0];
+        for (int j = 1; j < m; j++) dp[0][j] = dp[0][j - 1] + grid[0][j];
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+        System.out.print("Enter number of columns: ");
+        int m = sc.nextInt();
+        int[][] grid = new int[n][m];
+        System.out.println("Enter grid values:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+        int result = minPathSum(grid, n, m);
+        System.out.println("Minimum Path Sum: " + result);
+        sc.close();
+    }
+}
