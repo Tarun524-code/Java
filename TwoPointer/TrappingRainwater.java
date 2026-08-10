411960fb66507627a5c745e3926f40594539fb0b
Tarun <tarunsriramnambada@gmail.com>
2026-08-10 21:59:00 +0530

    Trapping Rainwater Problem using Two Pointer Technique

---
+++ TwoPointer/TrappingRainwater.java
@@ -0,0 +1,37 @@
+package TwoPointer;
+
+import java.util.Scanner;
+
+public class TrappingRainwater {
+    public static int trap(int[] height) {
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0, water = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) leftMax = height[left];
+                else water += leftMax - height[left];
+                left++;
+            } else {
+                if (height[right] >= rightMax) rightMax = height[right];
+                else water += rightMax - height[right];
+                right--;
+            }
+        }
+        return water;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elevation map:");
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        int result = trap(arr);
+        System.out.println("Total trapped water: " + result);
+        sc.close();
+    }
+}
+
